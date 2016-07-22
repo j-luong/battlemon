0b2f99943b2b91c2dbb8567fdabbc9c45c5cb275
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-22 10:02:54 +0100

    completed battlemon

--- app.rb
+++ app.rb
@@ -3,28 +3,29 @@ require './lib/player'
 require 'sinatra/base'
 
 class Battlemon < Sinatra::Base
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    $game = Game.new(Player.new(params[:player_1_name]),
-                     Player.new(params[:player_2_name]))
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     erb :play
   end
 
   post '/attack' do
-    @game = $game
     @game.attack(@game.victim)
     redirect '/play'
   end
-
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,14 @@ require_relative 'player'
 class Game
   attr_reader :players, :attacker, :victim
 
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+  
+  def self.instance
+    @game
+  end
+
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
     @attacker = player_1
